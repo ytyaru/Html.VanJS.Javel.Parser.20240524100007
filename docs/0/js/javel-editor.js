d44ca7e2cf99fae734798e5377ce32698561d4bd
ytyaru <yttry0@gmail.com>
2024-05-25 13:53:39 +0900

    Javel,Element,HTML相互変換実装

--- docs/0/js/javel-editor.js
+++ docs/0/js/javel-editor.js
@@ -1,32 +1,14 @@
 const {div, textarea, button} = van.tags
 class JavelEditor {
     constructor() {
-        //this.viewer = new SingleScreen() 
         this.parser = new JavelParser()
         this.manuscript = van.state('')
         this.textBlocks = van.derive(()=>this.parser.toBlocks(this.manuscript.val))
         this.htmls = van.derive(()=>this.parser.toHtml(this.textBlocks.val))
-        //this.htmls = van.derive(()=>{this.parser.toHtml(this.textBlocks.val);this.viewer.children = div(this.htmls);})
         this.size = van.derive(()=>this.parser.calcSize(this.htmls.val))
     }
     init() {
-        this.viewer = new SingleScreen() 
-        //van.derive(()=>this.viewer.children = this.htmls)
-        //van.derive(()=>this.viewer.children = div(this.htmls))
-        //van.derive(()=>this.viewer.children = [()=>div(this.htmls)])
-        //this.viewer.children = [()=>div(this.htmls)]
-        //this.viewer.children = ()=>div(this.htmls)
-        //this.viewer.children = div(this.htmls)
-        this.viewer.children = this.htmls
-        console.log(this.viewer.children)
-        console.log(this.viewer.el)
-        //van.add(document.body, div({style:()=>this.style()}, textarea({rows:5, cols:40, oninput:(e)=>this.manuscript.val=e.target.value}, ()=>this.manuscript.val), button({style:`word-break:break-all;padding:0;margin:0;line-height:1em;letter-spacing:0;`},()=>`${this.size.val}字`), ()=>div([div('AAAA'), div('BBBB')])))
-        //van.add(document.body, div({style:()=>this.style()}, textarea({rows:5, cols:40, oninput:(e)=>this.manuscript.val=e.target.value}, ()=>this.manuscript.val), button({style:`word-break:break-all;padding:0;margin:0;line-height:1em;letter-spacing:0;`},()=>`${this.size.val}字`), ()=>div(this.htmls)))
         van.add(document.body, div({style:()=>this.style()}, textarea({rows:5, cols:40, oninput:(e)=>this.manuscript.val=e.target.value}, ()=>this.manuscript.val), button({style:`word-break:break-all;padding:0;margin:0;line-height:1em;letter-spacing:0;`},()=>`${this.size.val}字`), ()=>div(this.htmls.val)))
-        //van.add(document.body, div({style:()=>this.style()}, textarea({rows:5, cols:40, oninput:(e)=>this.manuscript.val=e.target.value}, ()=>this.manuscript.val), button({style:`word-break:break-all;padding:0;margin:0;line-height:1em;letter-spacing:0;`},()=>`${this.size.val}字`), ()=>{console.log(this.viewer.children);div(this.viewer.children)}))
-        //van.add(document.body, div({style:()=>this.style()}, textarea({rows:5, cols:40, oninput:(e)=>this.manuscript.val=e.target.value}, ()=>this.manuscript.val), button({style:`word-break:break-all;padding:0;margin:0;line-height:1em;letter-spacing:0;`},()=>`${this.size.val}字`), ()=>this.viewer.el))
-        //van.add(document.body, div({style:()=>this.style()}, textarea({rows:5, cols:40, oninput:(e)=>this.manuscript.val=e.target.value}, ()=>this.manuscript.val), button({style:`word-break:break-all;padding:0;margin:0;line-height:1em;letter-spacing:0;`},()=>`${this.size.val}字`), ()=>div(this.viewer.el)))
-        //van.add(document.body, div({style:()=>this.style()}, textarea({rows:5, cols:40, oninput:(e)=>this.manuscript.val=e.target.value}, ()=>this.manuscript.val), button({style:`word-break:break-all;padding:0;margin:0;line-height:1em;letter-spacing:0;`},()=>`${this.size.val}字`), ()=>div({onwheel:(e)=>this.#onWheel(e), style:'writing-mode:vertical-rl;'}, this.htmls.val)))
         this.manuscript.val = `# 原稿《げんこう》
 
 　《《ここ》》に書いたテキストは下に表示《ひょうじ》されます。
@@ -58,7 +40,6 @@ class JavelEditor {
         if (0===width) { width = document.documentElement.clientWidth }
         if (0===height) { height = document.documentElement.clientHeight }
         const isLandscape = (height <= width)
-        // this.menu.isVertical = this.#isLandscape()
         const menuBlockSize = 16
         const uiWidth = (isLandscape) ? ((width - menuBlockSize) / 2) : width
         const uiHeight = (isLandscape) ? height : ((height - menuBlockSize) / 2)
@@ -69,78 +50,5 @@ class JavelEditor {
         const rows = sizes[1]
         this.fontSize(uiWidth)
         return `grid-template-columns:${columns};grid-template-rows:${rows};`
-    }
-}
-class SingleScreen {
-    constructor() {
-        this._children = van.state([])
-        this._w = van.state('100%')
-        this._h = van.state('100%')
-        this._writingMode = van.state('horizontal-tb') // vertical-rl
-        this._gridTemplateColumns = van.state('1fr')
-        this._gridTemplateRows = van.state('1fr')
-        this._overflowX = van.state('auto')
-        this._overflowY = van.state('auto')
-        this._textOrient = van.state('mixed') // upright
-        this._border = van.state('solid 1px #000')
-        this._wordBreak = van.state('normal')
-        this._fontSize = van.state(16)
-        this._div = van.tags.div({onwheel:(e)=>this.#onWheel(e), style:()=>`padding:0;margin:0;font-size:${this._fontSize.val}px;display:grid;display:grid;grid-template-columns:1fr;grid-template-rows:${this._gridTemplateRows.val};box-sizing:border-box;border:${this._border.val};writing-mode:${this._writingMode.val};overflow-x:${this._overflowX.val};overflow-y:${this._overflowY.val};text-orientation:${this._textOrient.val};word-break:${this._wordBreak.val};`}, ()=>div({style:`padding:0;margin:0;font-size:${this._fontSize.val}px;display:grid;display:grid;grid-template-columns:${this._gridTemplateColumns.val};grid-template-rows:${this._gridTemplateRows.val};box-sizing:border-box;`}, this.children))
-    }
-    get el() { return this._div }
-    get children( ) { return this._children.val }
-    set children(v) { this._children.val = v; this.resize(); }
-    get isVertical() { return !this.isHorizontal }
-    get isHorizontal() { return ('horizontal-tb'===this._writingMode.val) }
-    set isVertical(v) { this._writingMode.val = ((v) ? 'vertical-rl' : 'horizontal-tb'); this.resize(); }
-    set isHorizontal(v) { this._writingMode.val = ((v) ? 'horizontal-tb' : 'vertical-rl'); this.resize(); }
-    toggleWritingMode() {
-        this._writingMode.val = (('horizontal-tb'===this._writingMode.val) ? 'vertical-rl' : 'horizontal-tb')
-        this.#setOverflow()
-        this.resize()
-    }
-    get gridTemplateColumns( ) { return this._gridTemplateColumns.val }
-    set gridTemplateColumns(v) { this._gridTemplateColumns.val = v }
-    get gridTemplateRows( ) { return this._gridTemplateRows.val }
-    set gridTemplateRows(v) { this._gridTemplateRows.val = v }
-    #setOverflow() {
-        if ('horizontal-tb'===this._writingMode.val) {
-            this._overflowX.val = 'hidden'
-            this._overflowY.val = 'auto'
-            this._textOrient.val = 'mixed'
-        } else {
-            this._overflowX.val = 'auto'
-            this._overflowY.val = 'hidden'
-            this._textOrient.val = 'upright'
-        }
-        this.#setFontSize()
-    }
-    set wordBreak(val) { if (['normal','break-all','keep-all','break-word'].some(v=>v===val)) { this._wordBreak.val = val } }
-    get hasScrollBar() { return ((this.el.isHorizontal) ? this.el.scrollHeight > this.el.this.height : this.el.scrollWidth > this.el.this.width) }
-    get scrollSize() { return ((this.el.isHorizontal) ? this.el.offsetHeight - this.el.clientHeight : this.el.offsetWidth - this.el.clientWidth) }
-    #onWheel(e) {
-        if ('vertical-rl'===this._writingMode.val) {
-            if (Math.abs(e.deltaY) < Math.abs(e.deltaX)) return;
-            this._div.scrollLeft += e.deltaY;
-            e.preventDefault();
-        }
-    }
-    resize() { this.#setFontSize() }
-    //#setFontSize() { console.log('this.el:',this.el);this._fontSize.val = Font.calc(this.el); this.#setFontSizeElements(); }
-    #setFontSize() { console.log('this.el:',this.el);Font.resize();this._fontSize.val = Font.size; this.#setFontSizeElements(); }
-    #setFontSizeElements() {
-        console.log('#setFontSizeElements()')
-        //for (let el of this.el.querySelectorAll('textarea,select,input,button,label,legend')) {
-        for (let el of this.el.querySelectorAll('*[data-sid][data-eid]')) {
-            const tagName = el.tagName.toLowerCase()
-            if (!'div,textarea,select,input,button,label,legend'.split(',').some(v=>v===tagName)) { continue }
-            console.error(tagName)
-            Css.set('font-size', `${this._fontSize.val}px`, el)
-            console.log(el, this._fontSize.val)
-            if ('textarea'===tagName || ('div'===tagName)) {
-                Css.set('line-height', '1.7em', el)
-                Css.set('letter-spacing', '0.05em', el)
-            }
-        }
     }
 }
