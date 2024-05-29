5bc214340ef7ff6effa2378f68cf7cecfec600ad
ytyaru <yttry0@gmail.com>
2024-05-29 22:52:23 +0900

    6 fingerprint

--- docs/6/js/main.js
+++ docs/6/js/main.js
@@ -2,6 +2,17 @@ window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOMContentLoaded!!');
     const editor = new JavelEditor()
     editor.init()
+
+    const client = new ClientJS();
+    const fingerprint = client.getFingerprint();
+    console.log(fingerprint);
+    console.log(typeof fingerprint);
+    /*
+    new Fingerprint2().get(function(result, components){
+        console.log(result); //a hash, representing your device fingerprint
+        console.log(components); // an array of FP components
+    });
+    */
 });
 window.addEventListener('beforeunload', (event) => {
     console.log('beforeunload!!');
