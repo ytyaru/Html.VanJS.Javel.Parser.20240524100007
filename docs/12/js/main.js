909d0f2388f39666393b17d460a4d9bb63d55924
ytyaru <yttry0@gmail.com>
2024-06-02 18:03:19 +0900

    12 ファイル選択ダイアログから入力する

---
+++ docs/12/js/main.js
@@ -0,0 +1,22 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOMContentLoaded!!');
+    const app = new JavelWriter()
+//    app.init()
+
+    /*
+    const client = new ClientJS();
+    const fingerprint = client.getFingerprint();
+    console.log(fingerprint);
+    console.log(typeof fingerprint);
+    */
+    /*
+    new Fingerprint2().get(function(result, components){
+        console.log(result); //a hash, representing your device fingerprint
+        console.log(components); // an array of FP components
+    });
+    */
+});
+window.addEventListener('beforeunload', (event) => {
+    console.log('beforeunload!!');
+});
+
